1cd3f63f919519cf427e9fe2dbf1065278c35329
sjbuehner <sandi@sandiconsulting.com>
2015-09-24 11:56:03 -0700

    Completed Assignment 2 part 2

--- style.js
+++ style.js
@@ -29,7 +29,7 @@
 
     /*var sentence = ("The quick brown fox jumps over the lazy dog");
     var fullName = prompt("What is your full name?");
-    window.alert("The quick brown fox jumps over " + fullName);/*
+    console.log(sentence.replace("the lazy dog", fullName));*/
 
 /*7.	Create an application that stores the text “The quick brown fox jumps over the lazy dog” within a variable. Then, prompt the user for a word. Next, search for the word within the string that the user enters into the prompt. Use the alert method to display the result.*/
 
@@ -64,12 +64,12 @@
 		break;
  default: alert("Your word was not found.")
 }*/
+	
+/* Step 8:	Create an application that stores the text “The quick brown fox jumps over the lazy dog” within a variable called old_string. Then, use slice(), substr(), or substring() to extract the words “the lazy dog” from the old_string variable and store that result in a second variable called new_string. Use the alert method to display the uppercase result of new_string.*/
 
-/*8.	Create an application that stores the text “The quick brown fox jumps over the lazy dog” within a variable called old_string. Then, use slice(), substr(), or substring() to extract the words “the lazy dog” from the old_string variable and store that result in a second variable called new_string. Use the alert method to display the uppercase result of new_string.*/
-
-    /*var oldString ="The quick brown fox jumps over the lazy dog.";
-    var newString =(oldString.substring(31,44));
-	alert(newString);*/
+	/*var old_string = "The quick brown fox jumps over the lazy dog.";
+	var new_string = old_string.substring(31, 43);
+	alert(new_string.toUpperCase());*/
 
 /*9.	Create an application that stores the text “            THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG          ” within a variable. Make sure to add space before and after the text so that appears very similar to the text here. Use the alert method to display the lowercase result of the variable once the space has been trimmed off.*/
 
@@ -92,17 +92,17 @@
 /*1.	Create an application that prompts the user for a number. Regardless of whether they enter a negative or positive number, make sure to display the positive version of that number in the console window. If I enter -15 in the prompt and -15 is displayed in the console window, you did this one wrong.*/
 
         /*var userNumber = prompt("Please pick a number.");
-        alert(Math.abs(userNumber));*/
+        console.log(Math.abs(userNumber));*/
     
 /*2.	Create an application that prompts the user for a floating point value (decimal). Store the result of that input in a variable and then round it up to the nearest whole number. Display the result within a console window.*/
 
         /*var userNumber = prompt("Please pick a decimal number.");
-        alert(Math.ceil(userNumber));*/
+        console.log(Math.ceil(userNumber));*/
 
 /*3.	Create an application that prompts the user for a floating point value (decimal). Store the result of that input in a variable and then round it down to the nearest whole number. Display the result within a console window.*/
 
         /*var userNumber = prompt("Please pick a decimal number.");
-        alert(Math.floor(userNumber));*/
+       console.log(Math.floor(userNumber));*/
 
 /*4.	Create an application that prompts the user for 5 numbers. Ask them to comma delimit each number so you get 1,2,3,4,5 for example. Store the result of that input in a variable and then find the largest and smallest numbers in that list. Display both of those numbers within a console window.*/
 	/*var n = prompt("Pick a number");
@@ -110,39 +110,76 @@
 	var n2 = prompt("Pick another number");
 	var n3 = prompt("Pick another number");
 	var n4 = prompt("Pick one last number");
-	alert(Number(n) + ", " + Number(n1) + ", " + Number(n2) + ", " + Number(n3) + ", " + Number(n4));
-	alert(Math.min(n, n1, n2, n3, n4));
-	alert(Math.max(n, n1, n2, n3, n4));*/
+	console.log(Number(n) + ", " + Number(n1) + ", " + Number(n2) + ", " + Number(n3) + ", " + Number(n4));
+	console.log(Math.min(n, n1, n2, n3, n4));
+	console.log(Math.max(n, n1, n2, n3, n4));*/
         
 
 /*5.	Create an application that prompts the user for a number. Now find the square root of that number and display the result within a console window. */
 
         /*var userNumber = prompt ("Please pick a number.");
-        alert(Math.sqrt(userNumber));*/
+        console.log(Math.sqrt(userNumber));*/
 
 //Part 2 Date and Date Functions
 
 //6.	Create an application that gets the current date. Display that result within the console window.
 
         /*var d = new Date();
-        alert(d.getDate());*/
+       console.log(d.getDate());*/
         
 //7.	Create an application that gets the number of days in a month. Display that result within the console window.
-
-        
+	
+	/*var d =  new Date();
+	var m =  (d.getMonth());
+		if (m === 0 || m === 2 || m === 4 || m === 6 || m === 7 || m === 9 || m === 11) {
+		console.log("The month has 31 days");
+		}	else if (m === 3 || m === 5 || m === 8 || m === 10) {
+		console.log("The month has 30 days");
+		}	else {
+		console.log("The month has 28 days");
+		}*/
+		
 
 //8.	Create an application that gets the month name from a particular date. Display that result within the console window.
+
+	/*var d = new Date();
+	var m = d.getMonth();
+		if (m === 0) {
+		console.log("January");
+		}	else if (m === 1) {
+		console.log("February");
+		}	else if (m === 2) {
+		console.log("March");
+		}	else if (m === 3) {
+		console.log("April");
+		}	else if (m === 4) {
+		console.log("May");
+		}	else if (m === 5) {
+		console.log("June");
+		}	else if (m === 6) {
+		console.log("July");
+		}	else if (m === 7) {
+		console.log("August");}
+		else if (m === 8) {
+		console.log("September");}
+		else if (m === 9) {
+		console.log("Otober");}
+		else if (m === 10) {
+		console.log("November");}
+		else{
+		console.log("December");
+		}*/
 
 //9.	Create an application that tests whether a date is a weekend. Display that result within the console window.
 
     /* var d = new Date();
         var weekend = d.getDay();
       if (weekend == 6){
-                alert("Today is Saturday.");
+                console.log("Today is Saturday.");
             }  else if (weekend == 0) {
-                alert("Today is Sunday.");
+                console.log("Today is Sunday.");
             } else {
-                alert("Today is not a weekend.");
+                console.log("Today is not a weekend.");
             }*/
 
 /*10.	Create an application that gets yesterday’s day of the week. For instance, if today is Tuesday, the console window should display Monday.*/
@@ -185,19 +222,46 @@
                 alert("S");
             }*/
 
-/*Part 3 - Conditional Logic and Looping Operations
+//Part 3 - Conditional Logic and Looping Operations
 /*12.	Create an application that accepts two integers within two separate prompts. Then, display only the larger of the two within the console window.*/
 
   /*var firstNum = prompt("Please pick a number."); 
     var secondNum = prompt("Please pick a number."); 
     if (firstNum >= secondNum){
         console.log(firstNum);
-    }    else{
+		}	else{
         console.log(secondNum);
         };*/
 
 /*13.	Create an application that records the marks for the following five students. Then, these marks are used to determine the corresponding grade. All 5 students and their grades should be displayed within the console window. */
 
+//My code doesn't work, I think I'm on the right track, but can't figure out where I'm going wrong.  
+/*var nameFirst = ["Ursula", "Paul", "Henry", "Tabitha", "Lucy"];
+	for(var x in nameFirst){
+		console.log(nameFirst[x]);
+	}
+var grade = [80, 77, 88, 95, 68];
+	for(var x in grade){
+		console.log(grade[x]);
+	}
+
+switch (grade) {
+	case (grade < 60):
+  console.log(name + ":  F");
+	break;
+	case (grade <70):
+  console.log(name + ":  D");
+	break;
+	case (grade < 80):
+    window.console.log(name + ":  C");
+	break;
+	case (grade < 90):
+    window.console.log(name + ":  B");
+	break;
+	default:
+    window.console.log(name + ":  A");
+}*/
+	
 /*14.	Create a JavaScript for loop that iterates from 1 to 15. Each iteration should check if the current number is odd or even, and display a message within the console window.*/
 	/*for (x = 1; x <= 15; x++){
 			if (x % 2===0){
@@ -261,19 +325,44 @@
 			alert("The flip was heads and you chose heads. . . you win!");
 		}	
 			if(x=="tails" && coinFlip==0){
-			alert("The flip was heads but you chose tails, you lose!");
-		} 	if(x=="heads" && coinFlip==1){
+			alert("The flip was heads but you chose tails, you lose!");}
+			if(x=="heads" && coinFlip==1){
 			result = alert("The flip was tails but you chose heads, you lose.");
 		}	else{
 			result = alert ("The flip was tails and you chose tails. . . you win!");
- 
- 		}*/
+			}*/
 
 //Part 6 Coin Flip Redux
+		/*var coinFlip;
+		for(i = 1; i<=10; i++){
+			coinFlip = Math.round(Math.random());
+			if(coinFlip==0){
+				console.log("heads");
+			} else {
+				console.log("tails");	
+			}
+		}*/
 
 //Part 7 Coin Flip Streak
+		/*var coinFlip;
+		do{
+			coinFlip = Math.round(Math.random());
+			if (coinFlip==0){
+			console.log("heads");
+			}
+		}
+				
+			while(coinFlip==0);
+			console.log("tails");*/
+			
 
 //Part 8 Looping a Triangle
+	/*var i = 0;
+	var note = (" ");
+	while (i < 7) {
+		window.console.log(note += "#");
+		i++;
+		}*/
 
 //Part 9 Odd or Even
 	/*for (x = 1; x <= 15; x++){
